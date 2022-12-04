6ac2dc2874b82e5503b394c881182e5ffb9e4840
Anastasia1ukina <lukina02anastasiy@gmail.com>
2022-12-05 02:26:24 +0300

    First commit

---
+++ source/js/main.js
@@ -0,0 +1,40 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import './modules/modals/video';
+import './modules/modals/slider';
+import Inputmask from 'inputmask';
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  iosVhFix();
+
+  new Inputmask({
+    mask: '+7 (999) 999-99-99',
+    showMaskOnHover: false,
+  }).mask(document.querySelectorAll('[type="tel"]'));
+});
+
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
+// привязывайте js не на классы, а на дата атрибуты (data-validate)
+
+// вместо модификаторов .block--active используем утилитарные классы
+// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// выносим все в дата атрибуты
+// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+
+// для адаптивного JS используется matchMedia и addListener
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// используйте .closest(el)
---
+++ source/js/modules/modals/slider.js
@@ -0,0 +1,9 @@
+// new Swiper('.reviews__swiper', {
+//   navigation: {
+//     prevEl: '.reviews__button-prev',
+//     nextEl: '.reviews__button-next',
+//   },
+//   slidesPerView: 1,
+//   spaceBetween: 15,
+//   autoHeight: true,
+// });
